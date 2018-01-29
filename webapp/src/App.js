a807262b9c7b7915f721ab11fcdbd4f289ceac30
gregsakai <gregsakai@gmail.com>
2018-01-29 15:35:54 -0800

    init commit

---
+++ webapp/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div id="mainPage">
+        
+        <div id="mvtInfo">MOTOR VEHICKLE FEFT IS THE SICKSTH ITTERATION OF THE POPULAR CAR STEALING GAME MOTER VHIKKEL THEPHTD. ITS GOT AKSHIN PACKED CAR STEELING AND DRIVING. PLAY WITH YOURE FEIRNDS OR ALONE. YOU WILL HAVE LOTS OF FUNS.
+        <br />
+        <button id="mvtBut">ENTER</button>
+        </div>
+        
+        </div>
+        <div id="mvtLogo">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
